542aa60008ae2edbb873ffcd805a4c3b2cf647a2
Nick T. <73005126+nick701@users.noreply.github.com>
2026-07-24 12:35:33 +0200

    chore(release): update diffy to 0.6.1

--- Casks/diffy.rb
+++ Casks/diffy.rb
@@ -1,6 +1,6 @@
 cask "diffy" do
-  version "0.6.0"
-  sha256 "4276c76e193bffadb04133b27b4b4ce8e62f5247ee88cdb1011ec6ff2bfc029a"
+  version "0.6.1"
+  sha256 "851217b6f3f8fafb412d2ef77f29a90fc2a3564ec67c3dcdb255e6d7d0503056"
 
   url "https://github.com/nick701/diffy/releases/download/v#{version}/Diffy-#{version}.zip"
   name "Diffy"
