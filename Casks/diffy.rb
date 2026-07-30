a9f1b7c4ef8e00947163173f8e82fefbdcacb0c2
Nick T. <73005126+nick701@users.noreply.github.com>
2026-07-30 17:56:06 +0200

    chore(cask): update diffy to 0.8.1

--- Casks/diffy.rb
+++ Casks/diffy.rb
@@ -1,6 +1,6 @@
 cask "diffy" do
-  version "0.8.0"
-  sha256 "4aa570ea87b4b1951a30385dfd79ab26574fbfdec40e2a354c3d1ab91f88d9bc"
+  version "0.8.1"
+  sha256 "c89bd651962cf0400ad9a32d5c66e4ce85f1668e9da051d5fa7d15544d94d73e"
 
   url "https://github.com/nick701/diffy/releases/download/v#{version}/Diffy-#{version}.zip"
   name "Diffy"
